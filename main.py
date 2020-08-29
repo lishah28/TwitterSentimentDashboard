3828354e8354abe7bb6a3f514fc3f7788c7a77fb
lishah28 <70358404+lishah28@users.noreply.github.com>
2020-08-28 22:42:32 -0400

    Implemented intial features

--- main.py
+++ main.py
@@ -1,11 +1,35 @@
+import pandas as pd
+import numpy as np
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from tweepy import API
+from tweepy import OAuthHandler
+from tweepy import Stream
+from tweepy.streaming import StreamListener
+
+import twitter_api_info
+
+def authenticate_app():
+    auth = OAuthHandler(twitter_api_info.C_KEY, twitter_api_info.C_SECRET)
+    auth.set_access_token(twitter_api_info.A_KEY, twitter_api_info.A_SECRET)
+    return auth
+
+def create_dataframe(tweets):
+    df = pd.DataFrame(data = [tweet.user.screen_name for tweet in tweets], columns = ['User'])
+    df['Text'] = np.array([tweet.text for tweet in tweets])
+    df['Location'] = np.array([tweet.user.location for tweet in tweets])
+
+    return df
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('Hi Lisha')
+    auth = authenticate_app()
+    api = API(auth)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    tweets = api.search(q = "pokemon card", lang = "en", result_type = 'recent', count = 10)
+
+    df = create_dataframe(tweets)
+
+    print(df)
+
+
+
